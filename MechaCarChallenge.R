030c62b929a10921b204a529f5fb2d34e32768b5
Cailyn <cailynmiller@Cailyns-Air>
2021-04-18 07:54:17 -0500

    d3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,9 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil_table <- read.csv('Suspension_Coil.csv')
 total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
 lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
+# deliverable 3
+t.test(suspension_coil_table$PSI, mu=1500)
+t.test(subset(suspension_coil_table,Manufacturing_Lot == 'Lot1')$PSI, mu=1500)
+t.test(subset(suspension_coil_table,Manufacturing_Lot == 'Lot2')$PSI, mu=1500)
+t.test(subset(suspension_coil_table,Manufacturing_Lot == 'Lot3')$PSI, mu=1500)
