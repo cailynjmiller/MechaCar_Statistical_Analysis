1c1c0843fcced8cae5291c2a81c8a90d3e867e51
Cailyn <cailynmiller@Cailyns-Air>
2021-04-18 07:31:36 -0500

    d2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,10 @@
+# deliverable 1
 library("dplyr")
 mechacar_mpg_table <- read.csv(file='MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg_table)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg_table))
+
+# deliverable 2
+suspension_coil_table <- read.csv('Suspension_Coil.csv')
+total_summary <- suspension_coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
